2a327051f8670d3ab9dbb52c813a784c3996c291
isaac <issaktoo2@gmail,com>
2024-11-07 13:26:32 +0300

    changes

--- src/ShopKeeper/Keyboard.jsx
+++ src/ShopKeeper/Keyboard.jsx
@@ -1,28 +1,55 @@
 // Keyboard.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import './Keyboard.css';  // Import the CSS file for keyboard styles
 
 function Keyboard({ onKeyPress }) {
-    const keys = [
-        '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
-        'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 
-        'U', 'V', 'W', 'X', 'Y', 'Z'
-    ];
+  // The keys array, divided into pages
+  const allKeys = [
+    ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
+    ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'],
+    ['K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T'],
+    ['U', 'V', 'W', 'X', 'Y', 'Z', '*', '#'],
+  ];
 
-    return (
-        <div className="keyboard">
-            {keys.map(key => (
-                <button 
-                    key={key} 
-                    className="typewriter-button"  // Apply the typewriter-button class
-                    onClick={() => onKeyPress(key)}
-                >
-                    {key}
-                </button>
-            ))}
-        </div>
-    );
+  // State to keep track of the current page
+  const [currentPage, setCurrentPage] = useState(0);
+
+  // Get the keys for the current page
+  const keysToShow = allKeys[currentPage];
+
+  // Handle Previous Button Click
+  const handlePrevious = () => {
+    if (currentPage > 0) setCurrentPage(currentPage - 1); // Go to previous page
+  };
+
+  // Handle Next Button Click
+  const handleNext = () => {
+    if (currentPage < allKeys.length - 1) setCurrentPage(currentPage + 1); // Go to next page
+  };
+
+  return (
+    <div className="keyboard-container">
+      <div className="keyboard">
+        {keysToShow.map((key) => (
+          <button
+            key={key}
+            className="typewriter-button"
+            onClick={() => onKeyPress(key)}
+          >
+            {key}
+          </button>
+        ))}
+      </div>
+      <div className="pagination-buttons">
+        <button onClick={handlePrevious} disabled={currentPage === 0}>
+          Previous
+        </button>
+        <button onClick={handleNext} disabled={currentPage === allKeys.length - 1}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Keyboard;
